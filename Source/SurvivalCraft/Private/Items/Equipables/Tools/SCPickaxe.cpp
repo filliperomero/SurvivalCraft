be46f293907e19c12b1da5f5d66a2e5a9dca908a
filliperomero <fillipe.romero@gmail.com>
2023-12-25 22:59:57 -0700

    Add Stone Pickaxe with basic logic

---
+++ Source/SurvivalCraft/Private/Items/Equipables/Tools/SCPickaxe.cpp
@@ -0,0 +1,85 @@
+﻿// Copyright Fillipe Romero
+
+#include "Items/Equipables/Tools/SCPickaxe.h"
+#include "Character/SCCharacter.h"
+#include "HarvestingSystem/SCDestructibleHarvestable.h"
+#include "HarvestingSystem/SCLargeItem.h"
+#include "Items/Data/ResourceData.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "SurvivalCraft/SurvivalCraft.h"
+
+ASCPickaxe::ASCPickaxe()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ASCPickaxe::UseItem_Implementation(ASCCharacter* SCCharacter)
+{
+	// TODO: Better to use a Interface here, so we don't need to import SCCharacter
+	// We're using the same montage as the Hatchet
+	SCCharacter->PlayEquipableMontage(FName("Hatchet"));
+}
+
+void ASCPickaxe::Interact_Implementation(const FVector& LocationToCheck)
+{
+	TArray<AActor*> ActorsToIgnore;
+	TArray<AActor*> OutActors;
+	TArray<TEnumAsByte<EObjectTypeQuery>> TraceObjects;
+	
+	TraceObjects.Add(UEngineTypes::ConvertToObjectType(ECC_STRUCTURE));
+	ActorsToIgnore.Add(GetOwner());
+	
+	if (UKismetSystemLibrary::SphereOverlapActors(GetWorld(), LocationToCheck, 60.f, TraceObjects, nullptr, ActorsToIgnore, OutActors))
+	{
+		for (const auto& Actor : OutActors)
+		{
+			HarvestFoliage(15.f, Actor);
+		}
+	}
+}
+
+void ASCPickaxe::HarvestFoliage(float Damage, AActor* Target)
+{
+	if (!LargeItemsResourceDataTable) return;
+
+	// TODO: Improve this so we don't need to cast, we can create a interface to get the reference of the LargeItem
+	ASCLargeItem* LargeItem = Cast<ASCLargeItem>(Target);
+
+	if (LargeItem)
+	{
+		LargeItem->ReceiveDamage(Damage);
+
+		// TODO: Find a better way to identify this
+		FLargeItemInfo* LargeItemInfo = LargeItemsResourceDataTable->FindRow<FLargeItemInfo>(FName(*UKismetSystemLibrary::GetDisplayName(Target)), TEXT(""));
+
+		if (!LargeItemInfo) return;
+
+		if (LargeItem->GetHealth() > 0.f)
+		{
+			// TODO: Create a Interface to Add the Harvested Item so we don't need to Cast everytime
+			ASCCharacter* Character = Cast<ASCCharacter>(GetOwner());
+			
+			for (auto& GivenItem : LargeItemInfo->GivenItems)
+			{
+				Character->ServerAddHarvestedItem(GivenItem);
+			}
+		}
+		else
+		{
+			const FTransform SpawnTransform = Target->GetActorTransform();
+			
+			ASCDestructibleHarvestable* SpawnedDestructibleHarvestable = GetWorld()->SpawnActorDeferred<ASCDestructibleHarvestable>(
+				LargeItemInfo->DestructibleHarvestableClass,
+				SpawnTransform,
+				this,
+				nullptr,
+				ESpawnActorCollisionHandlingMethod::AlwaysSpawn
+			);
+			
+			Target->Destroy();
+
+			SpawnedDestructibleHarvestable->SetDirectionToFall(GetOwner()->GetActorForwardVector());
+			SpawnedDestructibleHarvestable->FinishSpawning(SpawnTransform);
+		}
+	}
+}
---
+++ Source/SurvivalCraft/Public/Items/Equipables/Tools/SCPickaxe.h
@@ -0,0 +1,26 @@
+﻿// Copyright Fillipe Romero
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/SCEquipableItem.h"
+#include "SCPickaxe.generated.h"
+
+UCLASS()
+class SURVIVALCRAFT_API ASCPickaxe : public ASCEquipableItem
+{
+	GENERATED_BODY()
+
+public:
+	ASCPickaxe();
+
+	/** EquipableInterface */
+	virtual void UseItem_Implementation(ASCCharacter* SCCharacter) override;
+	virtual void Interact_Implementation(const FVector& LocationToCheck) override;
+	/** EquipableInterface */
+	
+	void HarvestFoliage(float Damage, AActor* Target);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Resources")
+	TObjectPtr<UDataTable> LargeItemsResourceDataTable;
+};
