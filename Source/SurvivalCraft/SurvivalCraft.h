a23a7d6daa0e05e6c4f52cf095d7080b3f6b3259
filliperomero <fillipe.romero@gmail.com>
2024-01-15 15:39:55 -0700

    Add Ramp Building and fix wrong trace channel for ECC_TORCH_TRACE

--- Source/SurvivalCraft/SurvivalCraft.h
+++ Source/SurvivalCraft/SurvivalCraft.h
@@ -21,4 +21,5 @@
 #define ECC_LANDSCAPE_TRACE ECollisionChannel::ECC_GameTraceChannel12
 #define ECC_FOUNDATION_PLACEMENT_TRACE ECollisionChannel::ECC_GameTraceChannel13
 #define ECC_TORCH_TRACE ECollisionChannel::ECC_GameTraceChannel14
-#define ECC_WALL_TRACE ECollisionChannel::ECC_GameTraceChannel14
+#define ECC_WALL_TRACE ECollisionChannel::ECC_GameTraceChannel15
+#define ECC_RAMP_TRACE ECollisionChannel::ECC_GameTraceChannel16
