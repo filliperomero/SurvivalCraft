7fcc64a66c5880a8ae50089f67a5e48542ade3f0
filliperomero <fillipe.romero@gmail.com>
2024-01-15 17:05:56 -0700

    Add Torch

---
+++ Source/SurvivalCraft/Private/BuildingSystem/SCTorch.cpp
@@ -0,0 +1,47 @@
+﻿// Copyright Fillipe Romero
+
+#include "BuildingSystem/SCTorch.h"
+
+#include "Net/UnrealNetwork.h"
+#include "Particles/ParticleSystemComponent.h"
+
+ASCTorch::ASCTorch()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	FireEffect = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("FireEffect"));
+	FireEffect->SetupAttachment(GetMesh());
+	FireEffect->SetRelativeLocation(FVector(-30.f, -50.f, 50.f));
+	FireEffect->SetAutoActivate(false);
+}
+
+void ASCTorch::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ASCTorch, bIsTorchLit);
+}
+
+void ASCTorch::InteractEvent_Implementation(ASCCharacter* Character)
+{
+	bIsTorchLit = !bIsTorchLit;
+
+	ToggleTorch();
+}
+
+void ASCTorch::ToggleTorch()
+{
+	if (bIsTorchLit)
+	{
+		FireEffect->Activate();
+	}
+	else
+	{
+		FireEffect->Deactivate();
+	}
+}
+
+void ASCTorch::OnRep_IsTorchLit()
+{
+	ToggleTorch();
+}
---
+++ Source/SurvivalCraft/Public/BuildingSystem/SCTorch.h
@@ -0,0 +1,35 @@
+﻿// Copyright Fillipe Romero
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SCBuildable.h"
+#include "Interfaces/InteractInterface.h"
+#include "SCTorch.generated.h"
+
+UCLASS()
+class SURVIVALCRAFT_API ASCTorch : public ASCBuildable, public IInteractInterface
+{
+	GENERATED_BODY()
+
+public:
+	ASCTorch();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	/** Interact Interface */
+	virtual void InteractEvent_Implementation(ASCCharacter* Character) override;
+	/** Interact Interface */
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = VFX)
+	TObjectPtr<UParticleSystemComponent> FireEffect;
+
+private:
+	void ToggleTorch();
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_IsTorchLit, meta = (AllowPrivateAccess = true))
+	bool bIsTorchLit = false;
+
+	UFUNCTION()
+	void OnRep_IsTorchLit();
+};
